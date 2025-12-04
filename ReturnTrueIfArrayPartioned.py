5b3d5d3bb170f7fee8b45dd428d0d12114ad1672
Moien Bhat <bhatmoien34@gmail.com>
2025-12-04 21:22:22 +0530

    Add files via upload

---
+++ ReturnTrueIfArrayPartioned.py
@@ -0,0 +1,20 @@
+def canPartition(nums):
+    total = sum(nums)
+    if total % 2 != 0:
+        return False
+
+    target = total // 2
+    dp = set([0])
+
+    for num in nums:
+        new_dp = set(dp)
+        for s in dp:
+            if s + num == target:
+                return True
+            new_dp.add(s + num)
+        dp = new_dp
+
+    return target in dp
+
+
+print(canPartition([1,5,1,5]))   
