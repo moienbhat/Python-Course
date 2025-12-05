90d1f55856621c8af7f90b47f35e53a2bef715dd
Moien Bhat <bhatmoien34@gmail.com>
2025-12-05 20:56:37 +0530

    Add files via upload

---
+++ MaxProfitFromOneTransaction.py
@@ -0,0 +1,14 @@
+
+
+def maxProfit(prices):
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        min_price = min(min_price, price)
+        max_profit = max(max_profit, price - min_price)
+
+    return max_profit
+
+
+print(maxProfit([7,1,5,3,6,4]))
