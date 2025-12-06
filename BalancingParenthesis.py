9aa3f4dabe58fa57584b49c41389c086b8c30c38
Moien Bhat <bhatmoien34@gmail.com>
2025-12-06 20:42:04 +0530

    Add files via upload

---
+++ BalancingParenthesis.py
@@ -0,0 +1,17 @@
+def isValid(s):
+    stack = []
+    pairs = {')':'(', ']':'[', '}':'{'}
+
+    for ch in s:
+        if ch in "([{":
+            stack.append(ch)
+        else:
+            if not stack or stack[-1] != pairs[ch]:
+                return False
+            stack.pop()
+
+    return len(stack) == 0
+
+
+print(isValid("({[]})"))    # True
+print(isValid("([)]"))      # False
