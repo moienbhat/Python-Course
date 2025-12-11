aed7d11f4a6ce091204aee20daecaf7ab95b1f69
Moien Bhat <bhatmoien34@gmail.com>
2025-12-11 14:37:06 +0530

    Add files via upload

---
+++ QuickSortInPlace.py
@@ -0,0 +1,17 @@
+def quicksort(arr, low, high):
+    if low < high:
+        p = partition(arr, low, high)
+        quicksort(arr, low, p - 1)
+        quicksort(arr, p + 1, high)
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
